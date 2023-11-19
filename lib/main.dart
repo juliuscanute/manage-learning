c30dcd0ba1fb0eadc20f4151e6cc21cf0e5e7fd7
juliuscanute <juliuscanute@gmail.com>
2023-11-19 18:00:12 +1100

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  initializeFirebase();
   runApp(const MyApp());
+}
+
+void initializeFirebase() async {
+  await dotenv.load(fileName: ".env", isOptional: true);
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: dotenv.env['FIREBASE_API_KEY'] ??
+          const String.fromEnvironment('FIREBASE_API_KEY'),
+      authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN'] ??
+          const String.fromEnvironment('FIREBASE_AUTH_DOMAIN'),
+      projectId: dotenv.env['FIREBASE_PROJECT_ID'] ??
+          const String.fromEnvironment('FIREBASE_PROJECT_ID'),
+      storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ??
+          const String.fromEnvironment('FIREBASE_STORAGE_BUCKET'),
+      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ??
+          const String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID'),
+      appId: dotenv.env['FIREBASE_APP_ID'] ??
+          const String.fromEnvironment('FIREBASE_APP_ID'),
+      measurementId: dotenv.env['FIREBASE_MEASUREMENT_ID'] ??
+          const String.fromEnvironment('FIREBASE_MEASUREMENT_ID'),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
